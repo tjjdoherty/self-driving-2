7084334ed37d44f4157b7947da53c79463003a28
Thomas Doherty <107805731+tjjdoherty@users.noreply.github.com>
2024-02-24 12:29:22 -0500

    add and remove segments and points, including removing segments associated with removed point

--- js/math/graph.js
+++ js/math/graph.js
@@ -20,6 +20,14 @@ class Graph {
         return false;
     }
 
+    removePoint(point) {
+        const segs = this.getSegmentsWithPoint(point);
+        for (const seg of segs) {
+            this.removeSegment(seg); // this is removing every segment in segs, in which we collected the subset of segments that touch the point being removed
+        }        
+        this.points.splice(this.points.indexOf(point), 1); // splice method like with remove segment 
+    }
+
     addSegment(seg) {
         this.segments.push(seg);
     }
@@ -37,7 +45,17 @@ class Graph {
     }
 
     removeSegment(seg) {
-        this.segments.splice(this.segments.indexOf((seg), 1)); // splice is very powerful, with just two arguments it will remove the segment matching the index of seg, and only remove one item 
+        this.segments.splice(this.segments.indexOf(seg), 1); // splice is very powerful, with just two arguments it will remove the segment matching the index of seg, and only remove one item 
+    }
+
+    getSegmentsWithPoint(point) { // this method is just collecting any segment that touches the point which is being removed in removePoint (above). 
+        const segs = []; // creating an empty array to put the segments to be deleted in removePoint
+        for (const seg of this.segments) {
+            if (seg.includes(point)) {
+                segs.push(seg);
+            }
+        }
+        return segs;
     }
 
     draw(ctx) { // simple draw method: loop through each segment in the segments array and draw on the canvas context

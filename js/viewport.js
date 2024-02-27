a377055c2559670a6eae430f095d02658e66197c
Thomas Doherty <107805731+tjjdoherty@users.noreply.github.com>
2024-02-27 07:27:02 -0500

    implemented panning on right click hold, however the point drawing doesnt update mouse coordinates after panning

--- js/viewport.js
+++ js/viewport.js
@@ -4,6 +4,14 @@ class Viewport {
         this.ctx = canvas.getContext("2d");
 
         this.zoom = 1;
+        this.offset = new Point(0, 0); // initializing the location of where you are panning to on the canvas
+
+        this.pan = {
+            start: new Point(0,0),
+            end: new Point(0,0),
+            offset: new Point(0,0),
+            active: false
+        }
         
         this.#addEventListeners();
     }
@@ -16,7 +24,36 @@ class Viewport {
     }
 
     #addEventListeners() {
+        this.canvas.addEventListener("mousedown", this.#handleMouseDown.bind(this));
+        this.canvas.addEventListener("mouseup", this.#handleMouseUp.bind(this));
+        this.canvas.addEventListener("mousemove", this.#handleMouseMove.bind(this));
         this.canvas.addEventListener("mousewheel", this.#handleMouseWheel.bind(this));
+    }
+
+    #handleMouseDown(event) {
+        if (event.button == 2) {  // I'm implementing panning using right click hold, Left click = 0; middle = 1;
+            this.pan.start = this.getMouse(event);
+            this.pan.active = true;
+        }
+    }
+
+    #handleMouseMove(event) {
+        if (this.pan.active) {
+            this.pan.end = this.getMouse(event); // where is the mouse moving whilst being clicked?
+            this.pan.offset = subtract(this.pan.end, this.pan.start); // simple vector calculation to find the net move of the pan
+        }
+    }
+
+    #handleMouseUp(event) {
+        if (this.pan.active) {
+            this.offset = add(this.offset, this.pan.offset);
+            this.pan = {
+                start: new Point(0,0),
+                end: new Point(0,0),
+                offset: new Point(0,0),
+                active: false // we reset the pan to 0 after storing the net offset in this.viewport.offset, which will change the view position
+            }
+        }
     }
 
     #handleMouseWheel(event) {

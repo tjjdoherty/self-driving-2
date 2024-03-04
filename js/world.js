965dec0fa2da235456f719bf71704bbf28d8ec33
Thomas Doherty <107805731+tjjdoherty@users.noreply.github.com>
2024-03-03 23:00:13 -0500

    made outer polygon guides for buildings

--- js/world.js
+++ js/world.js
@@ -1,11 +1,21 @@
 class World {
-    constructor(graph, roadWidth = 100, roadRoundness = 3) {
+    constructor(graph, 
+        roadWidth = 100,
+        roadRoundness = 10,
+        buildingWidth = 150,
+        buildingMinLength = 150,
+        spacing = 50
+    ) {
         this.graph = graph;
         this.roadWidth = roadWidth;
         this.roadRoundness = roadRoundness;
+        this.buildingWidth = buildingWidth;
+        this.buildingMinLength = buildingMinLength;
+        this.spacing = spacing;
 
         this.envelopes = [];
         this.roadBorders = [];
+        this.buildings = [];
 
         this.generate();
     }
@@ -19,14 +29,37 @@ class World {
         }
 
         this.roadBorders = Polygon.union(this.envelopes.map((envelope) => envelope.poly));
+        this.buildings = this.#generateBuildings();
+    }
+
+    #generateBuildings() {
+        const tmpEnvelopes = [];
+        for (const seg of this.graph.segments) {
+            tmpEnvelopes.push(
+                new Envelope(
+                    seg,
+                    this.roadWidth + this.buildingWidth + this.spacing * 2, // making a much larger envelope to place buildings inside, alongside the roads
+                    this.roadRoundness
+                )
+            );
+        }
+
+        const guides = Polygon.union(tmpEnvelopes.map((envelope) => envelope.poly))
+        return guides;
     }
 
     draw(ctx) {
         for (const env of this.envelopes) {
-            env.draw(ctx);
+            env.draw(ctx, { fill: "#BBB", stroke: "#BBB", lineWidth: 15 });
+        }
+        for (const seg of this.graph.segments) {
+            seg.draw(ctx, { color: "white", width: 4, dash: [10, 10]}) // dashed lines in middle of roads
         }
         for (const seg of this.roadBorders) {
             seg.draw(ctx, {color: "white", width: 4 });
+        }
+        for (const building of this.buildings) {
+            building.draw(ctx);
         }
     }
 }

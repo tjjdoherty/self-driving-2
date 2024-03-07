7f28250fa37da983f02f0fc58001ae27e53f8f1e
Thomas Doherty <107805731+tjjdoherty@users.noreply.github.com>
2024-03-07 18:38:14 -0500

    added ceilings to buildings, floating rooftops currently

--- js/items/building.js
+++ js/items/building.js
@@ -5,6 +5,11 @@ class Building {
     }
 
     draw(ctx, viewPoint) {
+        const topPoints = this.base.points.map((point) => 
+            add(point, scale(subtract(point, viewPoint), this.heightCoef)) // topPoints just defining the rooftop corners by adding new points, scaled/offset from the viewpoint for the 3d effect
+        );
+        const ceiling = new Polygon(topPoints); // then we just create a new polygon with those four points added in the map method
         this.base.draw(ctx, { fill: "white", stroke: "#AAA" });
+        ceiling.draw(ctx, { fill: "white", stroke: "#AAA" });
     }
 }

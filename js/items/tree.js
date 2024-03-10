e0d902dffbebd0b36ed3e792a8a50a88cd88e9ed
Thomas Doherty <107805731+tjjdoherty@users.noreply.github.com>
2024-03-09 19:07:59 -0500

    slight adjustment to tree height

--- js/items/tree.js
+++ js/items/tree.js
@@ -1,5 +1,5 @@
 class Tree {
-    constructor(center, size, height = 15) {
+    constructor(center, size, height = 35) {
         this.center = center;
         this.size = size; // size of the base
         this.height = height; // by default its 0.06, i have cacti in a desert setting so they aren't very big
@@ -21,8 +21,10 @@ class Tree {
     draw(ctx, viewPoint) {
         const direction = normalize(subtract(this.center, viewPoint)); // normalize this vector to get direction - it's a vector between 0 and 1 in magnitude
         const dist = distance(this.center, viewPoint); // how much is the tree/cactus is offset? its a 0 -> 1 function telling us how much to contribute, 0 directly above, 1 far away from the side
-        const scaler = Math.atan(dist) / (Math.PI / 2); // arc tangent function given a positive value will output anything between 0 and PI / 2, so divide by PI / 2 and youll get btn 0 and 1.
+        const scaler = Math.atan(dist / 200) / (Math.PI / 2); // arc tangent function given a positive value will output anything between 0 and PI / 2, so divide by PI / 2 and youll get btn 0 and 1.
         
+        // arc tangent becomes very sensitive at short distances (close to 0 -> 0.5) so we divide dist / 200 to avoid the trees swinging quickly when they are directly over the viewPoint (sensitivity)
+
         const top = add(this.center, scale(direction, this.height * scaler)) // direction gives us which way to do the scaling, scaler tells us by how much relative to the height
 
         const levelCount = 5;
